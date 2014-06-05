bb91ab469703dfdaa7f7aa92578f9fe89a4d3ca0
Doug Hazell <doughazell@gmail.com>
2014-06-05 20:32:13 +0100

    Added method to get results from the specified player permutation (not just combination since there's an advantage with starting)

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -24,32 +24,14 @@ describe 'games' do
     puts Game::PLAYER_RESULTS_MAP
     
     totalGames = Result.all.count
-    puts "Total games \t\t\t\t#{totalGames}"
+    puts "\nTotal games \t\t\t\t#{totalGames}"
     
-    statewithresult = Result.where("statewithresult = ?", 1).count
-    puts "StateWithResult won \t\t\t#{statewithresult}"
-    
-    state = Result.where("state = ?", 1).count
-    puts "State won \t\t\t\t#{state}"
-    
-    SWRvSdraw = Result.where("state = ? and statewithresult = ?",0,0).count
-    puts "StateWithResult drew against State \t#{SWRvSdraw}"
-    
-    stateFirst = Result.where("first = ?","state").count
-    puts "\nState went first \t\t\t#{stateFirst}"
-    
-    if statewithresult + state + SWRvSdraw == totalGames
-      puts "[Well that figures]"
-    else
-      puts "[Doh!  Something went wrong there...]"
-    end
-    
-    random = Result.where("random != ?", nil).count
+    random = Result.where("random is not null").count
     puts "Random played \t\t\t\t#{random}"
     
   end
 
-  it "can run" do
+  it "BoardStateOnly vs BoardStateWithResult" do
 
     game_number.times.each do |number| 
       puts "\n*** GAME #{number+1} ***\n\n"
@@ -60,7 +42,7 @@ describe 'games' do
       click_button "Create Game"
       
       expect(page).to have_text("Game ended")
-#debugger
+
       notice = find("p#notice").text
       puts "\nNotice: #{notice}\n\n"
       path = page.current_path
@@ -68,6 +50,41 @@ describe 'games' do
 
       puts "Winner of game #{gameID} = #{Game::PLAYER_MAP[Game.find(gameID).game_outcome]}"
     end
+    
+    getResults(first: "state", second: "statewithresult")
+
+  end
+  
+  def getResults(args = {})
+    #puts args[:first]
+    #puts args[:second]
+    
+    player1 = Game::PLAYER_RESULTS_MAP.key(args[:first])
+    player2 = Game::PLAYER_RESULTS_MAP.key(args[:second])
+
+    subtotalGames = Result.where("first = ? and #{args[:first]} is not null and #{args[:second]} is not null",args[:first]).count
+    puts "\nSub-Total games \t\t\t\t\t\t\t#{subtotalGames}"
+
+    first = Result.where("#{args[:first]} = ?", 1).count
+    puts "#{player1} won \t\t\t\t\t\t#{first} [FIRST]"
+    
+    second = Result.where("#{args[:second]} = ?", 1).count
+    puts "#{player2} won \t\t\t\t\t#{second} [SECOND]"
+    
+    draw = Result.where("#{args[:first]} = ? and #{args[:second]} = ?",0,0).count
+    puts "#{player1} drew against #{player2} \t#{draw}"
+
+=begin    
+    stateFirst = Result.where("first = ?","state").count
+    puts "\nState went first \t\t\t#{stateFirst}"
+=end
+    
+    if first + second + draw == subtotalGames
+      puts "[Well that figures]"
+    else
+      puts "[Doh!  Something went wrong there...]"
+    end
+
   end
 
 end
